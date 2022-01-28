43ac74bff3e1bb9c03e1661ed271724c8d8c72a5
timceharris <timceharris@gmail.com>
2022-01-28 17:14:18 +0000

    configured app to use scss

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,8 @@
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="scss">
 
-#nav {
-  padding: 30px;
+@use 'assets/styles/main';
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>

0791c6a7f63841548b9c24a388a4b9de6e08e488
timceharris <timceharris@gmail.com>
2022-01-29 18:34:51 +0000

    added navbar for all pages + config for scss

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to='/'>
+      <img src="banner.png">
+      </router-link>
     </div>
+    <div class='router-view'>
     <router-view/>
+    </div>
   </div>
 </template>
 

7f158b7259dc94d15fce9612900f8e3ca780d331
JohnnyYuge <johnnyq@hotmail.fr>
2022-02-01 23:53:44 +0100

    style: code of conduct in the new website + styled

---
+++ src/views/CodeOfConduct.vue
@@ -0,0 +1,218 @@
+<template>
+  <div class="container">
+    <article class="coc-container">
+      <h1 class="has-text-centered">Code of Conduct</h1>
+      <h2>Our Community</h2>
+      <p>
+        It is important to us that Server vs Server (SvS) fosters a harassment-free environment. Thus, we have designed the following
+        Code of Conduct (CC) to outline guidelines and expectations that we have for everyone to help us reach
+        this. The CC also formalises repercussions for breaking these guidelines and provides a point of reference
+        when dealing with issues that may arise in a community of our size.
+      </p>
+      <p>You can find our full CC below. If you have any questions or concerns, you can leave them here.</p>
+      <p>Thank you for helping make this a welcoming, friendly community for everyone.</p>
+      <h2>Scope</h2>
+      <p>This CC applies to the following people:</p>
+      <ul>
+        <li>Staff / organisers</li>
+        <li>Guest speakers / interviewees</li>
+        <li>Competition participants</li>
+        <li>Partners of SvS</li>
+        <li>Members of SvS online spaces</li>
+        <li>Members of participating communities</li>
+      </ul>
+      <h3>SvS Spaces</h3>
+      <p>This CC applies to the following spaces:</p>
+      <ul>
+        <li>The SvS Twitch Channel</li>
+        <li>The SvS Discord</li>
+        <li>Public spaces of partnered servers</li>
+        <li>Social Media accounts managed by or that relate to SvS</li>
+        <li>
+          Code repositories, issue trackers and pull requests made against any SvS controlled GitHub organization
+        </li>
+        <li>
+          Interactions with official event accounts on all social media spaces, including comments made on:
+          <ul>
+            <li>Event live streams</li>
+            <li>Event video hosting services</li>
+            <li>Official event pages, posts, hashtags etc.</li>
+          </ul>
+        </li>
+        <li>Any other online space administered by SvS</li>
+      </ul>
+      <p>This CC applies to everyone in any of these official SvS online spaces.</p>
+      <p>Each online space listed above is encouraged to provide the following information to community members:</p>
+      <ul>
+        <li>Contact information for any administrators/moderators</li>
+        <li>
+          A welcome message with a link to this CC and the contact information for making an incident report:
+          <a
+            class="link"
+            href="https://svs-website.vercel.app/concerns"
+          >https://svs-website.vercel.app/concerns</a>
+        </li>
+      </ul>
+      <h3>Additional spaces</h3>
+      <h4>Participating Communities</h4>
+      <p>In addition to the SvS-managed spaces listed above, individuals within participating communities are held to the CC. It is the responsibility of moderators of these spaces to uphold these standards.</p>
+      <h4>Organisers</h4>
+      <p><b>“Organisers”</b> refer to both Event Staff or Partners.</p>
+      <p>
+        <b>“Event Staff” </b>are members who are directly associated with SvS and are members of any of the following
+        teams:
+      </p>
+      <ul>
+        <li>Event Coordination</li>
+        <li>Creative and Design</li>
+        <li>Social Media</li>
+        <li>Participant Outreach</li>
+        <li>Fundraising and Sponsorship Coordination</li>
+        <li>Live Stream and Tech</li>
+        <li>Moderation</li>
+      </ul>
+      <p>
+        <b>“Partners”</b> are those who are directly involved in the organisation of an event, but are not directly
+        part of the SvS staff team. These can include:
+      </p>
+      <ul>
+        <li>Communities/organisations that co-host an event</li>
+        <li>Charities</li>
+        <li>Sponsors</li>
+      </ul>
+      <p>Both kinds of event organisers are expected to:</p>
+      <ul>
+        <li>Provide a form of direct contact information to a representative of your organization</li>
+        <li>Uphold and enforce the CC everywhere - whether directly related to SvS or not</li>
+        <li>Manage events fairly without bias towards or against any participants / participating communities</li>
+      </ul>
+      <h2>Behaviours</h2>
+      <p>
+        Members of the SvS community are expected to be open, considerate, and respectful. The following behaviours
+        reinforce these values and contribute to a positive environment:
+      </p>
+      <ul>
+        <li>
+          Being open. Members of the community are open to collaboration, compromise, and are committed to transparency.
+          Focusing on what is best for the community. We're respectful of the processes set forth in the community, and we
+          work within them.
+        </li>
+        <li>
+          Acknowledging time and effort. We're respectful of the volunteer efforts that form the foundation of the SvS
+          community. We're thoughtful when addressing the efforts of others, keeping in mind that the labour was often
+          completed simply for the good of the community.
+        </li>
+        <li>
+          Being respectful of differing viewpoints and experiences. We're receptive to constructive comments and
+          criticism, as the experiences and skill sets of other members contribute to the whole of our efforts.
+          Showing empathy towards other community members. We're attentive in our communications, whether in person or
+          online, and we're tactful when approaching differing views.
+        </li>
+        <li>
+          Being considerate. Members of the community are considerate of their peers -- other SvS participants.
+          Being respectful. We're respectful of others, their positions, their skills, their commitments, and their
+          efforts.
+        </li>
+        <li>
+          Gracefully accepting constructive criticism. When we disagree, we are courteous in raising our issues.
+        </li><li>
+          Using welcoming and inclusive language. We're accepting of all who wish to take part in our activities,
+          fostering an environment where anyone can participate and everyone can make a difference.
+        </li><li>
+          Holding members of the community accountable to this CC.
+        </li>
+      </ul>
+      <h2>Standards</h2>
+      <p>
+        Every member of our community has the right to have their identity respected. The SvS community is dedicated to
+        providing a positive and welcoming environment and experience for everyone, regardless of age, gender identity
+        and expression, sexual orientation, disability, physical appearance, ethnicity, nationality, religious beliefs,
+        or socioeconomic status.
+      </p>
+      <h3>Inappropriate Behavior</h3>
+      <p>Examples of unacceptable behavior by participants include:</p>
+      <ul>
+        <li>Harassment of any participants in any form (including intimidation or stalking)</li>
+        <li>Logging or taking screenshots of online activity for harassment purposes</li>
+        <li>
+          Publishing other's private information, such as a physical or electronic address, without explicit permission
+          (“doxing”)
+        </li>
+        <li>Violent threats or language directed against another person</li>
+        <li>Creating additional online accounts in order to harass another person or circumvent a ban</li>
+        <li>Unwelcome sexual advances, languages, or imagery of any kind in online communities</li>
+        <li>
+          Insults, put-downs, or jokes that are based upon stereotypes, are exclusionary, hold others up for ridicule, or
+          are in any way designed to cause offence to a person based upon their identity
+        </li>
+        <li>Disruption of online community discussions, voice calls, or Livestream-based events</li>
+        <li>Unwelcome or inappropriate direct messaging of members without explicit consent</li>
+        <li>Continued one-on-one communication after requests to cease</li>
+        <li>Conduct that violates the Discord, Twitch, or any other platform’s Terms of Service</li>
+      </ul>
+      <blockquote>
+        <b>Disclaimer:</b> We recognise that the use of explicit language/content may be present within a musical/artistic context, as opposed to being used between participating individuals. This extension of artistic license does not override our overall standards and scope and will be carefully reviewed on a case-by-case basis.
+      </blockquote>
+      <h2>Enforcement</h2>
+      <p>
+        If a participant engages in behaviour that violates this CC, the SvS Moderation team may take action that they
+        deem appropriate, including warning the offender or expulsion from the community and community events. The full
+        list of consequences for inappropriate behavior is listed in the Enforcement Procedures.
+      </p>
+      <h3>Enforcement Procedures</h3>
+      <ul>
+        <li>
+          We have a zero-tolerance policy regarding discrimination, harassment, or any such targeted behaviours that
+          continue after warning/expressions of discontent.
+        </li>
+        <li>
+          For smaller infractions, up to three warnings will be given, after which proper disciplinary action will be
+          taken.
+        </li>
+        <li>
+          Participants, staff, and partners who are found violating the CC will be barred from organizing or competing in
+          future SvS events.
+        </li>
+        <li>
+          Event moderators will enforce this code regardless of the event. They will work with the Event Coordination team
+          and the parties involved to take a fair course of action.
+        </li>
+      </ul>
+
+      <blockquote>
+        If you believe that someone is violating the CC, or have any other concerns, please either use this <a
+          class="link"
+          href="https://svs-website.vercel.app/concerns"
+        >anonymous
+          form</a> or contact a member of our Event Moderation or Event Staff team immediately.
+      </blockquote>
+      <h2>License</h2>
+      <blockquote>
+        This CC is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
+        <a
+          class="link"
+          href="https://creativecommons.org/licenses/by-sa/3.0/"
+        >(https://creativecommons.org/licenses/by-sa/3.0/)</a>
+      </blockquote>
+      <h2>Attributions</h2>
+      <p>
+        This CC was adapted from the Python Community CC <a
+          class="link"
+          href="https://www.python.org/psf/conduct/"
+        >(https://www.python.org/psf/conduct/)</a>
+      </p>
+      <p>
+        This anti-harassment policy is based on the example policy from the Geek Feminism wiki, created by the Ada
+        Initiative and other volunteers. <a
+          class="link"
+          href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment"
+        >(http://geekfeminism.wikia.com/wiki/Conference_anti-harassment)</a>
+      </p>
+      <p>Additional new language and modifications were created by SvS.</p>
+    </article>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped lang='scss'></style>

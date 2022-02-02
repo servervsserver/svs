93217ec5ec863378898604a859446880acddd0f3
JohnnyYuge <johnnyq@hotmail.fr>
2022-02-02 22:23:51 +0100

    style: changed scroll behavior so it comes back to top

--- src/router/index.js
+++ src/router/index.js
@@ -126,7 +126,14 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return {
+      x: 0,
+      y: 0,  
+      behavior: 'smooth'
+    }
+  }
 })
 
 export default router

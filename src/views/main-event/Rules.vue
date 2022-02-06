a7dc100d616a252d29c1adecc2d5d3a6adafcaa0
JohnnyYuge <johnnyq@hotmail.fr>
2022-02-06 01:29:52 +0100

    feat: rules page + gradient-background available (separated class so it can be attached on anything)

--- src/views/main-event/Rules.vue
+++ src/views/main-event/Rules.vue
@@ -1,8 +1,149 @@
 <template>
-  <div class="rules">
-    <h1>
-      Rules
-    </h1>
+  <div class="container">
+    <article class="rules-container">
+      <h1>
+        SvS Competition Rules
+      </h1>
+      <ol>
+        <li>
+          <h2>Each server will make an EP that meets the following criteria:</h2>
+          <ol>
+            <li>
+              Must consist of at least 3 tracks (there is no upper limit) and 1 album cover for the whole EP.
+            </li>
+            <li>
+              Must be between 10 and 15 minutes long. This is a hard limit and will be strictly enforced, so take this into consideration while making creative decisions.
+            </li>
+            <li>
+              Must be based on the theme provided at the start of the competition.
+            </li>
+            <li>
+              Bonus tracks and other additional auditory content will not be accepted.
+            </li>
+            <li>
+              Servers <em>may</em> include 1 video/visualizer file to accompany the entire EP, if desired.
+            </li>
+            <li>
+              During the competition and as part of the final submission, no participant may receive compensation for their contribution to a song or composition, its production, or engineering.
+            </li>
+          </ol>
+        </li>
+        <li>
+          <h2>Track criteria:</h2>
+          <ol>
+            <li>
+              Must include musical contributions (ie composition, recordings, engineering - not feedback or “inspiration”) from at least 3 server members.
+            </li>
+            <li>
+              Must not be started before the official start of competition (no prior work allowed).
+            </li>
+            <li>
+              Must not contain any full external acapellas or instrumentals.
+            </li>
+            <li>
+              May use samples and extra assets (this is not counted as a contribution however).
+            </li>
+            <li>
+              Must not be a cover or remix of an existing song.
+            </li>
+          </ol>
+        </li>
+        <li>
+          <h2>Submission Criteria:</h2>
+          <ol>
+            <li>
+              Each EP must be submitted for playback for the stream as a ZIP file containing:
+              <ol>
+                <li>
+                  Each submitted track in its own mp3 file.
+                </li>
+                <li>
+                  An image file containing the cover art as a 3000x3000px PNG, GIF, or JPG/JPEG.
+                </li>
+                <li>
+                  A text file (.txt) containing the names of the contributors to each track and cover art and a record/example of their contribution; this is in addition to filling out the same information in the submission form.
+                </li>
+                <li>
+                  If applicable, a text file containing the full lyrics to each track.
+                </li>
+              </ol>
+            </li>
+            <li>
+              Track files must be in 320kbps mp3 format; tracks will be normalised during the listening festival livestream, so it is strongly advised to aim for -14 integrated LUFS when mastering  (as is the loudness standard for Spotify) to avoid any issues during livestream playback.
+            </li>
+            <li>
+              Track files must be named “Server Name - Track # - Track Name”
+            </li>
+            <li>
+              The EP must be submitted through our website before the deadline to be counted.
+            </li>
+            <li>
+              You must also submit a Youtube, Soundcloud, Audius, or Bandcamp link to your EP alongside your ZIP. This can be a private/unlisted link if you prefer, but there must be a way to stream your submission available by the deadline.
+            </li>
+            <li>
+              <strong>
+                By submitting an EP to the competition, you give SvS permission to use your EP as part of any of our future livestreams (and therefore VODs).
+              </strong>
+            </li>
+          </ol>
+        </li>
+        <li>
+          <h2>Application of Rules:</h2>
+          <ol>
+            <li>
+              If there is any question about the competition rules, how to interpret them, or how to apply them, at least 3 SvS event staff will make a decision as fairly as possible. No organiser, admin, or mod may participate in a decision directly related to servers they participate in.
+            </li>
+            <li>
+              Failure to follow the submission rules above will result in a server being disqualified from the ballot for awards (this does not necessitate suspension from the competition).
+            </li>
+            <li>
+              Failure to follow our <a href="/code-of-conduct">code of conduct</a> may result in a server being suspended or banned from the competition outright.
+            </li>
+          </ol>
+        </li>
+        <li>
+          <h2>Award Voting:</h2>
+          <ol>
+            <li>
+              Every member of the participating servers will be allowed to vote for awards after the festival via our website; More details will be provided in the future.
+            </li>
+            <li>
+              Servers MAY NOT vote for any of their own works. Doing so will lead to immediate disqualification.
+            </li>
+            <li>
+              Some awards that will be up for grabs include:
+              <ol>
+                <li>
+                  Favourite EP
+                </li>
+                <li>
+                  Favourite interpretation of theme (EP)
+                </li>
+                <li>
+                  Favourite Single
+                </li>
+                <li>
+                  Favourite Vocal (Track)
+                </li>
+                <li>
+                  Favourite Instrumental (Track)
+                </li>
+                <li>
+                  Favourite Cover Art
+                </li>
+                <li>
+                  Favourite Visualizer (Optional)
+                </li>
+              </ol>
+            </li>
+          </ol>
+        </li>
+      </ol>
+      <blockquote>
+        <strong>Disclaimers:</strong><br>
+        Participation in the competition and any related creative processes are completely voluntary. You will not receive financial compensation for your contributions.
+      </blockquote>
+    </article>
   </div>
 </template>
 

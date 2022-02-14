601af1c7d43f731605839d443aef77df536fa0fb
JohnnyYuge <johnnyq@hotmail.fr>
2022-02-14 01:50:14 +0100

    feat: staff crew cards

---
+++ src/components/StaffCard.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="staff-card flex-container shadow-depth-1 flat-background">
+    <div class="avatar-container">
+      <img
+        class="avatar"
+        :src="staff.avatarUrl"
+        alt="Avatar"
+      >
+    </div>
+    <div>
+      <div class="side-by-side">
+        <h2 class="name">
+          {{ staff.name }}
+        </h2>
+        <p> {{ staff.pronouns }} </p>
+      </div>
+      <h5> {{ staff.role }} </h5>
+      <p class="bodytext">
+        {{ staff.description }}
+      </p>
+
+      <div class="center-side-by-side">
+        <div>
+          <p class="socials-text">
+            <i
+              class="fab fa-discord"
+              style="font-size: 1.5em;"
+            />
+            {{ staff.discordTag }}
+          </p>
+        </div>
+
+        <div v-if="staff.extraSocialsLinks.twitter">
+          <p class="socials-text">
+            <i
+              class="fab fa-twitter"
+              style="font-size: 1.5em;"
+            />
+            @{{ staff.extraSocialsLinks.twitter }}
+          </p>
+        </div>
+
+        <div v-if="staff.extraSocialsLinks.instagram">
+          <p class="socials-text">
+            <i
+              class="fab fa-instagram"
+              style="font-size: 1.5em;"
+            />
+            <a
+              :href="'https://instagram.com/' + staff.extraSocialsLinks.instagram"
+            >
+              @{{ staff.extraSocialsLinks.instagram }}
+            </a>
+          </p>
+        </div>
+        <!-- <i
+          class="fab fa-twitter"
+          style="font-size: 1.5em;"
+        />
+        <p class="socials-text"> @cutealicem </p> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { StaffCard } from "@/models/staff-card"
+
+export default {
+  props: {
+    staff: {
+      type: StaffCard,
+      default: () => new StaffCard()
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+
+.staff-card {
+  border-radius: 5px;
+
+  &.flex-container {
+    display: flex;
+    color: #FFFADE;
+    align-items: center;
+  }
+
+  .avatar-container {
+    padding: 10px;
+  }
+
+  .avatar {
+    border-radius: 50%;
+    border: 0px solid #FFFADE;
+    box-shadow: 0px 0px 1px 1px #FFFADE;
+    /* margin-right: 20px; */
+    max-width: 100px;
+  }
+
+  .side-by-side {
+    display: flex;
+    align-items: baseline;
+    height: 43px;
+  }
+
+  .center-side-by-side {
+    display: flex;
+    align-items: center;
+  }
+
+  h2 {
+    margin: 0px;
+  }
+
+  h5 {
+    margin: 0px;
+    font-weight: normal;
+    color: #FFBA5E;
+    text-transform: capitalize;
+  }
+
+  .bodytext {
+    margin: 0px;
+    padding-top: 10px;
+    font-size: 14px;
+  }
+
+  .name {
+    padding-right: 6px;
+  }
+
+  .socials-text {
+    padding-left: 5px;
+    padding-right: 20px;
+    font-size: 12px;
+
+    a {
+      text-decoration: none;
+    }
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -10,9 +10,53 @@
       <brand-name-short />  is  a  Discord-based  music-making  event  that  began  Spring 2020 in response to the COVID-19 pandemic. Our objective  is  to  unite  the  rich  variety  of  online  music  communities  that  have  been  growing  on  Discord  through  the  simple shared goal of making music. By the end of the festival, each  server  that  participates  ends  with  purely  collaborative  work showcasing the skills and creativity of all the musicians who  contributed  their  time  and  effort.  Their  work  is  then  shared with all participants in a live Twitch festival at the end of  the  event,  which  also  acts  as  a  charity  fundraiser  event.
     </p>
     <h1>Who are the peps?</h1>
-    <p>Who are the staff ...</p>
+    <div class="columns is-multiline">
+      <div
+        v-for="member in staffMembers"
+        :key="member.vueId"
+        class="column is-6"
+      >
+        <staff-card :staff="member" />
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import StaffCardComponent from "@/components/StaffCard.vue"
+import { StaffCard } from "@/models/staff-card"
+
+export default {
+  components: {
+    'staff-card': StaffCardComponent
+  },
+  data () {
+    return {
+      staffMembers: [
+        new StaffCard(
+          "Benji",
+          "he/they",
+          "Event coordination",
+          "Hi! I'm Benji and I'm a music composer. I've been helping out with SvS since SvS II, and I enjoy helping run a competition many people enjoy.",
+          "Benji#7542",
+          null,
+          { twitter: 'cutealicem' }
+        ),
+        new StaffCard(
+          "Jiway",
+          "he/him",
+          "Tech",
+          "What's up slappers?",
+          "Jiway#7289",
+          "/staff/jiway/avatar.jpg",
+          { instagram: 'jiway_jy' }
+        ),
+        new StaffCard()
+      ]
+    }
+  }
+}
+</script>
 
 <style scoped lang='scss'>
 </style>

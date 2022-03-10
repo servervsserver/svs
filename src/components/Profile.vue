0d6f77864a41cb060afe840472e3b21fb05d0dfb
servervsserver <officialservervsserver@gmail.com>
2022-03-10 18:46:10 +0100

    mar10

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,22 +1,30 @@
 <template>
-  <div>
-    <img
-      :src="profile.icon"
-      alt=""
-      class="image"
-    >
+  <div class="container">
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image">
+              <img :src="profile.icon" alt="Placeholder image" />
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">{{ profile.name }}</p>
+            <p class="subtitle is-6">{{ profile.tag }}</p>
+            <span class="tag" v-if="profile.isStaff">#Staff </span>
+            <span class="tag" v-if="profile.isLeader">#Leader </span>
+          </div>
+        </div>
 
-    {{ profile.name }} |
-    {{ profile.tag }} |
-    {{ profile.isStaff }} | 
-    {{ profile.isLeaders }} |
-    <button
-      class="button"
-      @click="pullData"
-    >
+        <div class="content">Member of :</div>
+      </div>
+    </div>
+
+    <button class="button" @click="pullData">
       <span class="icon">
         <i class="fas fa-sync" />
-      </span> <span> Re-sync Profile</span>
+      </span>
+      <span> Re-sync Profile</span>
     </button>
   </div>
 </template>
@@ -27,30 +35,33 @@
 const axios = require("axios");
 
 export default {
-    data(){
-        return{
-            profile:{icon:"https://i.pravatar.cc/150"}
-        }
+  data() {
+    return {
+      profile: {
+        icon: "https://i.pravatar.cc/150",
+        name: "Loading...",
+        tag: "One moment pls.",
+      },
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      setTimeout(() => {
+        this.profile.name = "Any Moment now...";
+        this.pullData();
+      }, 2500);
+    });
+  },
+  methods: {
+    pullData() {
+      let storedProfile = this.$svsAuth.getData();
+      console.log(storedProfile);
+      if (storedProfile) {
+        this.profile = storedProfile;
+      }
     },
-    mounted(){
-         this.$nextTick(() => {
-       setTimeout(() => {
-         this.pullData();
-       }, 1000); 
-      });
-    },
-    methods:{
-        pullData(){
-let storedProfile = this.$svsAuth.getData();
-console.log(storedProfile);
-            if(storedProfile){
-              this.profile = storedProfile;
-            }
-            }
-
-
-    },
-}
+  },
+};
 </script>
 
 
@@ -63,7 +74,6 @@ console.log(storedProfile);
 
 
 <style lang="sass" scoped>
-
 </style>
 
 
